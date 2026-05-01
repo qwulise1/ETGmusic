695e9b3d7588be4fa448de464b752ab36fd5abb3
root <root@localhost.localdomain>
2026-05-01 07:35:00 +0000

    Fix settings divider import

--- lib/pages/settings/sections/accounts.dart
+++ lib/pages/settings/sections/accounts.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart' show Divider, ListTile, Wrap;
+import 'package:flutter/material.dart' show ListTile, Wrap;
 
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
